91f2d9579eb1974b38e1d2e80aec6bba0986d1b6
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2023-04-07 21:45:36 +0300

    all components are done

---
+++ client/src/app/components/signup/signup-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup.component';
+
+const routes: Routes = [
+  { path: 'signup', component: SignupComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class SignupRoutingModule { }
