47c0c02e1a9469bd08a4f3bda391bd71ad786737
minigar <dd8556205@gmail.com>
2023-04-09 11:39:36 +0300

    remove useless error key

--- server/src/controllers/errorKeys/TitleErrorKey.ts
+++ server/src/controllers/errorKeys/TitleErrorKey.ts
@@ -3,8 +3,6 @@ export const TitleErrorKey = {
   NAME_AREADY_EXIST: 'That name already exist, please rename',
   BAD_NAME_VALIDATION:
     'Name must contain only latin a-Z, 0-9 and symbols: _:"!@&-.`?^$#№+=-~',
-  NAME_MUST_CONTAINS_AT_LEAST_1_LETTER:
-    'Name must contains at least 1 latin letter',
   TYPES_BAD_VALIDATION:
     'Titles types must be array MANGA or MANHWA or MAMHUA or COMICS!',
   STATUS_BAD_VALIDATION:
